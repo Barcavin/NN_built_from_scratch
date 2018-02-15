a0637bfdface3f1240b09f50d1905bd309c8f79b
Kevin Dong <kevindong@wirelessprv-10-192-178-112.near.illinois.edu>
2018-02-14 19:10:14 -0600

    example code

---
+++ example.py
@@ -0,0 +1,13 @@
+import network
+import load
+from activation import *
+
+train,val,test = load.load_data()
+
+my_network = network.NeuralNetwork([28*28,500,300,10],[Relu(),Sigmoid(),Tanh()])
+my_network.train(train,0.01,30,20,0,val,"save.pkl")
+
+print("Test accuracy:{0}".format(my_network.eval(test)))
+
+new_network = network.NeuralNetwork([28*28,500,300,10],[Relu(),Sigmoid(),Tanh()])
+new_network.restore("save.pkl")
