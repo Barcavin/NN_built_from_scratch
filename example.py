ca6fc382a5c105cfe67d81f64af05bee5ead6959
barcavin <kevindong1994@gmail.com>
2018-02-22 22:54:13 +0000

    feasible

--- example.py
+++ example.py
@@ -4,10 +4,10 @@ from activation import *
 
 train,val,test = load.load_data()
 
-my_network = network.NeuralNetwork([28*28,500,300,10],[Relu(),Sigmoid(),Tanh()])
+my_network = network.NeuralNetwork([28*28,100,50,10],[Relu(),Sigmoid(),Tanh()])
 my_network.train(train,0.01,30,20,0,val,"save.pkl")
 
 print("Test accuracy:{0}".format(my_network.eval(test)))
 
-new_network = network.NeuralNetwork([28*28,500,300,10],[Relu(),Sigmoid(),Tanh()])
+new_network = network.NeuralNetwork([28*28,100,50,10],[Relu(),Sigmoid(),Tanh()])
 new_network.restore("save.pkl")
